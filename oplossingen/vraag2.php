267942c11a16cd43c9a954be8a26cd82760939ba
Vincent Bremer <vbremer89@gmail.com>
2012-09-20 13:41:44 +0200

    Added vraag2

---
+++ oplossingen/vraag2.php
@@ -0,0 +1,16 @@
+<?php
+$limit = 100;
+
+for ($i = 1; $i <= $limit; $i++):
+	if(($i % 3) == 0 && ($i % 5) == 0):
+		echo $i . ' - FizzBuzz<br />';	
+	elseif(($i % 3) == 0):
+		echo $i . ' - Fizz<br />';
+	elseif(($i % 5) == 0):
+		echo $i . ' - Buzz<br />';
+	else:
+		echo $i . '<br />';
+	endif;
+endfor;
+
+?>
